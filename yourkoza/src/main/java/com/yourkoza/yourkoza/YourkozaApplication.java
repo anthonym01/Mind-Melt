8d8d10938466b60bb875d8a71d5fcd3b6e9ea335
Samuel A. Matheson <samuelmatheson15@gmail.com>
2024-03-15 20:00:45 -0500

    spring boot

---
+++ yourkoza/src/main/java/com/yourkoza/yourkoza/YourkozaApplication.java
@@ -0,0 +1,13 @@
+package com.yourkoza.yourkoza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourkozaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourkozaApplication.class, args);
+	}
+
+}
---
+++ yourkoza/src/test/java/com/yourkoza/yourkoza/YourkozaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourkoza.yourkoza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourkozaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
