0fd3aeaf2522ca6c7f17fffebddbce5a83c2d2c2
Samuel A. Matheson <samuelmatheson15@gmail.com>
2024-03-15 23:23:54 -0500

    templates

--- Yourkoza/readme.md
+++ Yourkoza/readme.md
@@ -1,7 +1,12 @@
 # Dependencies  
 
-- pip install ply
+- ply
+- venv
+- Flask
 
 ## Settup
+
 - python3 -m venv .venv
+- . .venv/bin/activate
+- pip install Flask
 - pip install ply
--- Yourkoza/yourkoza.py
+++ Yourkoza/yourkoza.py
@@ -1,4 +1,5 @@
 # Yet another interprited language using ply
+# flask --app yourkoza run --host=0.0.0.0 --debug
 
 port = 8080;
 
@@ -6,6 +7,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}!"
