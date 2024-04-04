41524cc53baee094932cfedb0e64c389a50f946a
Samuel A. Matheson <samuelmatheson15@gmail.com>
2024-04-04 18:16:52 -0500

    Refactor precedence rules and grammar rules in yacc.py

--- Yourkoza2/yacc.py
+++ Yourkoza2/yacc.py
@@ -4,10 +4,29 @@ import ply.yacc as yacc
 from lexTokens import *
 
 precedence = (
-    ('nonassoc', 'IS_LESS_THAN', 'IS_GREATER_THAN','IS_EQUAL_TO','IS_NOT_EQUAL_TO', 'GREATER_THAN_OR_EQUAL_TO','LESS_THAN_OR_EQUAL_TO'),  # fix maybe
-    ('left', 'PLUS', 'MINUS'),
-    ('left', 'TIMES', 'DIVIDE'),
-    ('right', 'POWER'),
+    (
+        'nonassoc',
+        'IS_LESS_THAN',
+        'IS_GREATER_THAN',
+        'IS_EQUAL_TO',
+        'IS_NOT_EQUAL_TO',
+        'GREATER_THAN_OR_EQUAL_TO',
+        'LESS_THAN_OR_EQUAL_TO'
+    ),  # fix maybe
+    (
+        'left',
+        'PLUS',
+        'MINUS'
+    ),
+    (
+        'left',
+        'TIMES',
+        'DIVIDE'
+    ),
+    (
+        'right',
+        'POWER'
+    ),
 )
 
 # Grammar rules
@@ -61,7 +80,9 @@ def p_comment(p):
     p[0] = ('comment', p[1])
 
 def p_conditional(p):
-    '''conditional : IF condition THEN statements else_statements_opt END'''
+    """
+    conditional : IF condition THEN statements else_statements_opt END
+    """
     p[0] = ('conditional', p[2], p[4], p[5])
 
 def p_else_statements_opt(p):
@@ -74,12 +95,14 @@ def p_else_statements_opt(p):
 
 
 def p_condition(p):
-    '''condition : expression IS_LESS_THAN expression
-                 | expression IS_EQUAL_TO expression
-                 | expression IS_GREATER_THAN expression
-                 | expression IS_NOT_EQUAL_TO expression
-                 | expression GREATER_THAN_OR_EQUAL_TO expression
-                 | expression LESS_THAN_OR_EQUAL_TO expression'''
+    """"
+    condition : expression IS_LESS_THAN expression
+              | expression IS_EQUAL_TO expression
+              | expression IS_GREATER_THAN expression
+              | expression IS_NOT_EQUAL_TO expression
+              | expression GREATER_THAN_OR_EQUAL_TO expression
+              | expression LESS_THAN_OR_EQUAL_TO expression
+    """
     p[0] = ('condition', p[1], p[2], p[3])
 
 def p_expression(p):
@@ -113,11 +136,12 @@ def p_expression(p):
         p[0] = p[1]
 
 def p_term(p):
-    '''term : term TIMES factor
-            | term DIVIDE factor
-            | IDENTIFIER
-            | factor
-    '''
+    """
+    term : term TIMES factor
+         | term DIVIDE factor
+         | IDENTIFIER
+         | factor
+    """
     if len(p) == 4:
         if p[2] == '*':
             p[0] = p[1] * p[3]
@@ -127,16 +151,20 @@ def p_term(p):
         p[0] = p[1]
 
 def p_factor(p):
-    '''factor : NUMBER
-              | LPAREN expression RPAREN
-    '''
+    """
+    factor : NUMBER
+           | LPAREN expression RPAREN
+           | REAL
+    """
     if len(p) == 4:
         p[0] = p[2]
     else:
         p[0] = p[1]
 
 def p_input(p):
-    '''input : LET IDENTIFIER EQUAL SHOW IDENTIFIER LPAREN STRING_LITERAL RPAREN'''
+    """
+    input : LET IDENTIFIER EQUAL SHOW IDENTIFIER LPAREN STRING_LITERAL RPAREN
+    """
     p[0] = ('input', p[2], p[9])
 
 def p_display(p):
@@ -162,33 +190,48 @@ def p_display(p):
     p[0] = ('display', p[2])
 
 def p_loop(p):
-    '''loop : FOR IDENTIFIER IN list DO statements
-            | WHILE condition DO statements'''
+    """
+    loop : FOR IDENTIFIER IN list DO statements
+         | WHILE condition DO statements
+         | DO statements WHILE condition
+         | DO statements UNTIL condition
+    """
     p[0] = ('loop', p[1], p[2], p[4], p[6])
 
 
 def p_list(p):
-    '''list : LBRACKET expression_list RBRACKET
-            | empty'''
+    """
+    list : LBRACKET expression_list RBRACKET
+         | empty
+    """
     if len(p) == 4:
         p[0] = ('list', p[2])
 
 
 def p_expression_list(p):
-    '''expression_list : expression
-                       | expression_list COMMA expression'''
+    """
+    expression_list : expression
+                    | expression_list COMMA expression
+                    | empty
+    """
     if len(p) == 2:
         p[0] = ('expression_list', p[1])
     else:
         p[0] = ('expression_list', p[1], p[3])
 
 def p_function(p):
-    '''function : FUNCTION IDENTIFIER LPAREN parameters RPAREN LBRACE statements RBRACE return_statement_opt RBRACE'''
+    """
+    function : FUNCTION IDENTIFIER LPAREN parameters RPAREN LBRACE statements RBRACE return_statement_opt RBRACE
+    """
     p[0] = ('function', p[2], p[4], p[7], p[8])
 
 def p_parameters(p):
-    '''parameters : IDENTIFIER
-                  | parameters COMMA IDENTIFIER'''
+    """
+    parameters : IDENTIFIER
+               | parameters COMMA IDENTIFIER
+               | empty
+               | IDENTIFIER COLON type
+    """
     if len(p) == 2:
         p[0] = ('parameters', p[1])
     else:
